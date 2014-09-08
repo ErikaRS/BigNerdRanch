14f507a1a31a3d18b7a4ffff4c1abcddb01f505a
Erika RS <erika.rice.scherpelz@gmail.com>
2014-09-08 15:18:01 -0700

    tabs to spaces

--- GeoQuiz/app/src/main/java/com/erikars/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/erikars/geoquiz/CheatActivity.java
@@ -11,65 +11,65 @@ import android.util.Log;
 import android.os.Build;
 
 public class CheatActivity extends Activity {
-	private static final String TAG = CheatActivity.class.toString();
+  private static final String TAG = CheatActivity.class.toString();
 
-	public static final String EXTRA_ANSWER = "com.erikars.geoquiz.answer";
-	public static final String EXTRA_ANSWER_SHOWN = "com.erikars.geoquiz.answer_shown";
+  public static final String EXTRA_ANSWER = "com.erikars.geoquiz.answer";
+  public static final String EXTRA_ANSWER_SHOWN = "com.erikars.geoquiz.answer_shown";
 
-	private static final String KEY_ANSWER_SHOWN = "answer_shown";
+  private static final String KEY_ANSWER_SHOWN = "answer_shown";
 
-	private TextView mAnswerTextView;
+  private TextView mAnswerTextView;
 
-	private boolean mAnswer;
-	private boolean mAnswerShown = false;
+  private boolean mAnswer;
+  private boolean mAnswerShown = false;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_cheat);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_cheat);
 
-		if (savedInstanceState != null) {
-			mAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN, false);
-		}
-		Log.d(TAG, "onCreate() mAnswerShown: " + mAnswerShown);
+    if (savedInstanceState != null) {
+      mAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN, false);
+    }
+    Log.d(TAG, "onCreate() mAnswerShown: " + mAnswerShown);
 
-		mAnswer = getIntent().getBooleanExtra(EXTRA_ANSWER, false);
-		mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+    mAnswer = getIntent().getBooleanExtra(EXTRA_ANSWER, false);
+    mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
 
-		setAnswerShownResult();
+    setAnswerShownResult();
 
-		if (mAnswerShown) {
-			showAnswer();
-		}
+    if (mAnswerShown) {
+      showAnswer();
+    }
 
-		Button showAnswerButton = (Button) findViewById(R.id.showAnswerButton);
-		showAnswerButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View unused) {
-					showAnswer();
-				}
-			});
-			
-		TextView buildVersionTextView = (TextView) findViewById(R.id.buildVersion);
-		buildVersionTextView.setText(getResources().getString(R.string.build_version, Build.VERSION.SDK_INT));
-	}
+    Button showAnswerButton = (Button) findViewById(R.id.showAnswerButton);
+    showAnswerButton.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View unused) {
+          showAnswer();
+        }
+      });
+      
+    TextView buildVersionTextView = (TextView) findViewById(R.id.buildVersion);
+    buildVersionTextView.setText(getResources().getString(R.string.build_version, Build.VERSION.SDK_INT));
+  }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		Log.d(TAG, "onSaveInstanceState() mAnswerShown: " + mAnswerShown);
-		outState.putBoolean(KEY_ANSWER_SHOWN, mAnswerShown);
-	}
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    Log.d(TAG, "onSaveInstanceState() mAnswerShown: " + mAnswerShown);
+    outState.putBoolean(KEY_ANSWER_SHOWN, mAnswerShown);
+  }
 
-	private void showAnswer() {
-		mAnswerTextView.setText(mAnswer ? R.string.true_button : R.string.false_button);
-		mAnswerShown = true;
-		setAnswerShownResult();
-	}
+  private void showAnswer() {
+    mAnswerTextView.setText(mAnswer ? R.string.true_button : R.string.false_button);
+    mAnswerShown = true;
+    setAnswerShownResult();
+  }
 
-	private void setAnswerShownResult() {
-		Intent i = new Intent();
-		i.putExtra(EXTRA_ANSWER_SHOWN, mAnswerShown);
-		setResult(RESULT_OK, i);
-	}
+  private void setAnswerShownResult() {
+    Intent i = new Intent();
+    i.putExtra(EXTRA_ANSWER_SHOWN, mAnswerShown);
+    setResult(RESULT_OK, i);
+  }
 }
