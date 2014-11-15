7e28b51d4b85d6ece7a4de75f582030d8b44c339
ErikaRS <erika.rice.scherpelz@gmail.com>
2014-11-15 13:01:52 -0800

    Initial setup of NerdLauncher exercise

---
+++ NerdLauncher/app/src/main/java/com/erikars/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,14 @@
+package com.erikars.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+  @Override
+  protected Fragment createFragment() {
+    // TODO: Implement this method
+    return new Fragment();
+  }
+
+}
