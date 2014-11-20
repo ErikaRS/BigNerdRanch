c8c6adad8087b94b26f8fab7827c974833ae360e
ErikaRS <erika.rice.scherpelz@gmail.com>
2014-11-20 15:57:45 -0800

    Adding debugging logs

--- NerdLauncher/app/src/main/java/com/erikars/nerdlauncher/SingleFragmentActivity.java
+++ NerdLauncher/app/src/main/java/com/erikars/nerdlauncher/SingleFragmentActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 @SuppressWarnings("WeakerAccess")
 public abstract class SingleFragmentActivity extends ActionBarActivity {
+  private static final String TAG = SingleFragmentActivity.class.getSimpleName();
+  
   protected abstract Fragment createFragment();
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "Creating activity with fragment");
+    
     setContentView(getLayoutResId());
 
     FragmentManager fm = getSupportFragmentManager();
